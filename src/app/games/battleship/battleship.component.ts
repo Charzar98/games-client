e7ac3b31baff1715da719f52d1de7d3e95ca37bf
CThoele98 <cthoele1398@gmail.com>
2025-03-08 14:00:32 -0600

    Initial commit

---
+++ src/app/games/battleship/battleship.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-battleship',
+  templateUrl: './battleship.component.html',
+  styleUrls: ['./battleship.component.scss'],
+  standalone: true
+})
+export class BattleshipComponent {
+
+}
