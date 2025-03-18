394f152190bd7911ebc23364409514fd5b9aa78c
Charlie <cthoele1398@gmail.com>
2025-03-18 01:10:28 -0500

    Reformat, cleanup

--- src/app/games/wordle/wordle.component.ts
+++ src/app/games/wordle/wordle.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ExternalRequestsService } from '../../services/external-requests.service';
 import { MatInputModule } from '@angular/material/input';
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import { AppComponent } from "./app/app.component";
-import { bootstrapApplication } from "@angular/platform-browser";
-import { provideHttpClient } from "@angular/common/http";
+import { AppComponent } from './app/app.component';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(),
     provideAnimations()
-]
+  ]
 }).catch(err => console.error(err));
